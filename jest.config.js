f003ac5e2f960d5ee202497c1e6ec975f4d9b48c
Adam Hegedus <adamhegedus@vivaldi.net>
2021-04-28 00:20:52 +0200

    Configure test coverage

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,7 @@ module.exports = {
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    '**/*.ts',
+  ]
 };

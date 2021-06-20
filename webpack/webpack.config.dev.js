0add150cfc93dd6b9775a3eeff58b48e107afb6e
Adam Hegedus <adamhegedus@vivaldi.net>
2021-06-20 10:41:53 +0200

    Separate webpack configuration

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const config = require('./webpack.config.shared');
+
+module.exports = {
+  ...config,
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(process.cwd(), 'dist'),
+    compress: true,
+    port: 8080,
+  },
+  optimization: {
+    moduleIds: 'named',
+    chunkIds: 'named',
+    mangleExports: false,
+    nodeEnv: 'development',
+    flagIncludedChunks: false,
+    occurrenceOrder: false,
+    concatenateModules: false,
+    splitChunks: {
+      hidePathInfo: false,
+      minSize: 10000,
+      maxAsyncRequests: Infinity,
+      maxInitialRequests: Infinity,
+    },
+    emitOnErrors: true,
+    checkWasmTypes: false,
+    minimize: false,
+    removeAvailableModules: false,
+    // moduleIds: 'deterministic',
+    // runtimeChunk: 'single',
+    // splitChunks: {
+    //   cacheGroups: {
+    //     vendor: {
+    //       test: /[\\/]node_modules[\\/]/,
+    //       name: 'vendors',
+    //       chunks: 'all',
+    //     },
+    //   },
+    // },
+  },
+};
